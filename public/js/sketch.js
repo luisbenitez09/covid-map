6209bc6760bb224908475d83b9f575e48b700cf4
mariameza99 <54654158+mariameza99@users.noreply.github.com>
2021-06-12 14:37:01 -0600

    grafica infectados

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -5,13 +5,18 @@ var plot2 = undefined;
 function setup() {
     var parent = document.getElementById("graphic");
 
-    console.log(parent.clientWidth);
-    console.log(parent.clientHeight);
     var canvas = createCanvas(parent.clientWidth, parent.clientHeight);
     canvas.parent("graphic");
 
+    var stateNames = [];
+
+    for (const property in covidData.State) {
+        //console.log(`${property}: ${covidData.State[property].infected}`);
+        stateNames.push(property);
+    }
+
     var points, i;
-    // Create the first plot
+    // Grafica infectados
     plot1 = new GPlot(this);
     plot1.setPos(0, 0);
     plot1.setMar(60, 70, 40, 70);
@@ -19,31 +24,25 @@ function setup() {
     plot1.setAxesOffset(4);
     plot1.setTicksLength(4);
 
-    // Create the second plot with the same dimensions
-    plot2 = new GPlot(this);
-    plot2.setPos(plot1.getPos());
-    plot2.setMar(plot1.getMar());
-    plot2.setDim(plot1.getDim());
-    plot2.setAxesOffset(4);
-    plot2.setTicksLength(4);
+    var infectedNumbers = [];
+
+    for (const property in covidData.State) {
+        //console.log(`${property}: ${covidData.State[property].infected}`);
+        infectedNumbers.push(covidData.State[property].infected);
+    }
 
     // Prepare the points
-    points = [];
+    infectedPoints = [];
 
-    for (i = 0; i < 50; i++) {
-        points[i] = new GPoint(i, 30 + 10 * this.noise(i * 0.1));
+    for (i = 0; i < 32; i++) {
+        infectedPoints[i] = new GPoint(i, infectedNumbers[i]);
     }
 
     // Set the points, the title and the axis labels
-    plot1.setPoints(points);
+    plot1.setPoints(infectedPoints);
     plot1.setTitleText("COVID-19 por Estado");
     plot1.getYAxis().setAxisLabelText("Infectados");
     plot1.getXAxis().setAxisLabelText("Estados");
-
-    plot2.getRightAxis().setAxisLabelText("Fallecidos");
-
-    // Make the right axis of the second plot visible
-    plot2.getRightAxis().setDrawTickLabels(true);
 
     // Activate the panning (only for the first plot)
     plot1.activatePanning();
@@ -51,7 +50,7 @@ function setup() {
     noLoop();
 }
 
-function draw () {
+function draw() {
     background("fff");
 
     // Draw the plot
@@ -64,22 +63,4 @@ function draw () {
     plot1.drawLines();
     plot1.endDraw();
 
-    // Change the second plot vertical scale from Celsius to Kelvin
-    plot2.setYLim(celsiusToKelvin(plot1.getYLim()));
-
-    // Draw only the right axis
-    plot2.beginDraw();
-    plot2.drawRightAxis();
-    plot2.endDraw();
-    
-}
-
-function celsiusToKelvin(celsius) {
-    var kelvin = [];
-
-    for (var i = 0; i < celsius.length; i++) {
-        kelvin[i] = 273.15 + celsius[i];
-    }
-
-    return kelvin;
 }
